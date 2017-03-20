1d3486e5249b5ca3376a1fc19116e68776858647
davidizzy <davidizroy@gmail.com>
2017-03-20 16:54:50 -0500

    Including changes from video 5 (of 6)

--- main.py
+++ main.py
@@ -17,21 +17,43 @@
 import webapp2
 import random
 
+
+def getRandomFortune():
+    #make list of possible fortunes
+    fortunes = [
+        "I see much code in your future",
+        "Consider eating more fortune cookies",
+        "You have tamed the mighty Python, now you must free it onto the Great Spider's Web",
+        "Look under your seat",
+        "Never listen when a cookie tells you to look under your seat",
+        "Mmmm cookies"
+    ]
+
+    #randomly select one of the fortunes
+    index = random.randint(0,(len(fortunes)-1))
+
+    return fortunes[index]
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         header = "<h1>Fortune Cookie</h1>"
-        lucky_number = random.randint(1, 100)
-        number_sentence = 'Your lucky number: ' + str(lucky_number)
+
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+
+        lucky_number = "<strong>" + str(random.randint(1, 100))  + "</strong>"
+        number_sentence = 'Your lucky number: ' + lucky_number
         number_paragraph = "<p>" + number_sentence + "</p>"
-        self.response.write(header + number_paragraph)
 
-class LoginHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write("Thanks for trying to login!")
+        cookie_again_button = "<a href='.'><button>Another cookie please!</button></a>"
 
-routes = [
-    ('/', MainHandler),
-    ('/login', LoginHandler)
-]
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+        self.response.write(content)
 
-app = webapp2.WSGIApplication(routes, debug=True)
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
